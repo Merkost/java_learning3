4c2c2cc1403820dd91e8eede6ba7574296e9aea6
Kostya <KonstantinLikes@gmail.com>
2020-10-01 13:15:53 +1000

    HW4

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,64 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+    static Object mon = new Object();
+    static final int NUM = 5;
+    static volatile char currentLetter = 'A';
+
+    public static void main(String[] args) {
+
+        new Thread(()-> {
+            try {
+                for (int i = 0; i < NUM; i++) {
+                    synchronized (mon) {
+                        while (currentLetter != 'A') {
+                            mon.wait();
+                        }
+                        System.out.print("A");
+                        currentLetter = 'B';
+                        mon.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(()-> {
+            try {
+                for (int i = 0; i < NUM; i++) {
+                    synchronized (mon) {
+                        while (currentLetter != 'B') {
+                            mon.wait();
+                        }
+                        System.out.print("B");
+                        currentLetter = 'C';
+                        mon.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(()-> {
+            try {
+                for (int i = 0; i < NUM; i++) {
+                    synchronized (mon) {
+                        while (currentLetter != 'C') {
+                            mon.wait();
+                        }
+                        System.out.print("C\n");
+                        currentLetter = 'A';
+                        mon.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+
 
     }
+}
